f4e98845233c6b107c432eea0eff3217b5dbd3bf
oneflinyfish <a_flying_fish@outlook.com>
2025-07-16 11:22:08 +0800

    upload

---
+++ export_image_onnx.py
@@ -0,0 +1,209 @@
+import sys
+
+sys.path.insert(0, "sam2")
+
+import torch
+from sam2.build_sam import build_sam2
+from sam2.sam2_image_predictor import SAM2ImagePredictor
+from PIL import Image
+import numpy as np
+from torch import nn
+import os
+import onnx
+
+device = torch.device("cuda")
+onnx_path = "models"
+
+# large
+sam2_checkpoint = "./sam2/checkpoints/sam2.1_hiera_large.pt"
+model_cfg = "configs/sam2.1/sam2.1_hiera_l.yaml"
+
+# # tiny
+# sam2_checkpoint = "./sam2/checkpoints/sam2.1_hiera_tiny.pt"
+# model_cfg = "configs/sam2.1/sam2.1_hiera_t.yaml"
+
+sam2_model = build_sam2(model_cfg, sam2_checkpoint, device=device)
+predictor = SAM2ImagePredictor(sam2_model)
+
+
+@torch.no_grad()
+def export_forward_image(
+    onnx_name="forward_image.onnx", simplify_onnx=True, override=False
+):
+    global predictor, onnx_path
+    os.makedirs(onnx_path, exist_ok=True)
+
+    ori_forward = predictor.model.forward
+    predictor.model.forward = predictor.model.inference_image
+    predictor.model.eval()
+
+    save_path = os.path.join(onnx_path, onnx_name)
+    if os.path.exists(save_path) and not override:
+        print(f"skip gen {save_path}")
+        return
+
+    input_img = torch.randn(1, 3, 1024, 1024).to(device)
+    torch.onnx.export(
+        predictor.model,
+        input_img,
+        save_path,
+        export_params=True,
+        opset_version=17,
+        do_constant_folding=True,
+        input_names=["image"],
+        output_names=[
+            "vision_features",
+            "vision_pos_enc0",
+            "vision_pos_enc1",
+            "vision_pos_enc2",
+            "backbone_fpn0",
+            "backbone_fpn1",
+            "backbone_fpn2",
+        ],
+        dynamic_axes={
+            "image": {0: "N"},
+            "vision_features": {0: "N"},
+            "vision_pos_enc0": {0: "N"},
+            "vision_pos_enc1": {0: "N"},
+            "vision_pos_enc2": {0: "N"},
+            "backbone_fpn0": {0: "N"},
+            "backbone_fpn1": {0: "N"},
+            "backbone_fpn2": {0: "N"},
+        },
+    )
+
+    predictor.model.forward = ori_forward
+
+    if simplify_onnx:
+        from onnxsim import simplify
+
+        onnx_model = onnx.load(save_path)  # load onnx model
+        model_simp, check = simplify(onnx_model)
+        assert check, "Simplified ONNX model could not be validated"
+        onnx.save(model_simp, save_path.replace(".onnx", "_opt.onnx"))
+
+
+@torch.no_grad()
+def export_image_set_image(
+    onnx_name="image_set_image.onnx", simplify_onnx=True, override=False
+):
+    '''
+    
+    '''
+    global predictor, onnx_path
+    os.makedirs(onnx_path, exist_ok=True)
+
+    ori_forward = predictor.model.forward
+    predictor.model.forward = predictor.model.inference_image_for_set_image
+    predictor.model.eval()
+
+    save_path = os.path.join(onnx_path, onnx_name)
+    if os.path.exists(save_path) and not override:
+        print(f"skip gen {save_path}")
+        return
+
+    input_img = torch.randn(1, 3, 1024, 1024).to(device)
+    torch.onnx.export(
+        predictor.model,
+        input_img,
+        save_path,
+        export_params=True,
+        opset_version=17,
+        do_constant_folding=True,
+        input_names=["image"],
+        output_names=[
+            "feature0",
+            "feature1",
+            "feature2",
+        ],
+        dynamic_axes={
+            "image": {0: "N"},
+            "feature0": {0: "N"},
+            "feature1": {0: "N"},
+            "feature2": {0: "N"},
+        },
+    )
+
+    predictor.model.forward = ori_forward
+
+    if simplify_onnx:
+        from onnxsim import simplify
+
+        onnx_model = onnx.load(save_path)  # load onnx model
+        model_simp, check = simplify(onnx_model)
+        assert check, "Simplified ONNX model could not be validated"
+        onnx.save(model_simp, save_path.replace(".onnx", "_opt.onnx"))
+
+
+@torch.no_grad()
+def export_mask_encoder(
+    onnx_name="image_mask_encoder.onnx", simplify_onnx=True, override=False
+):
+    global predictor, onnx_path
+    os.makedirs(onnx_path, exist_ok=True)
+
+    ori_forward = predictor.model.sam_mask_decoder.forward
+    predictor.model.sam_mask_decoder.forward = (
+        predictor.model.sam_mask_decoder.inference_predict_masks
+    )
+    predictor.model.sam_mask_decoder.eval().to(device=device)
+
+    save_path = os.path.join(onnx_path, onnx_name)
+    if os.path.exists(save_path) and not override:
+        print(f"skip gen {save_path}")
+        return
+
+    src = torch.randn(1, 256, 64, 64).to(device)
+    tokens = torch.randn(1, 9, 256).to(device)
+    pos_src = torch.randn(1, 256, 64, 64).to(device)
+    high_res_feature0 = torch.randn(1, 32, 256, 256).to(device)
+    high_res_feature1 = torch.randn(1, 64, 128, 128).to(device)
+
+    torch.onnx.export(
+        predictor.model.sam_mask_decoder,
+        (
+            src,
+            tokens,
+            pos_src,
+            high_res_feature0,
+            high_res_feature1,
+        ),
+        save_path,
+        export_params=True,
+        opset_version=17,
+        do_constant_folding=True,
+        input_names=[
+            "src",
+            "tokens",
+            "pos_src",
+            "high_res_feature0",
+            "high_res_feature1",
+        ],
+        output_names=["masks", "iou_pred", "mask_tokens_out", "object_score_logits"],
+        dynamic_axes={
+            "src": {0: "N"},
+            "tokens": {0: "N",1:"L"},
+            "pos_src": {0: "N"},
+            "high_res_feature0": {0: "N"},
+            "high_res_feature1": {0: "N"},
+            "masks": {0: "N"},
+            "iou_pred": {0: "N"},
+            "mask_tokens_out": {0: "N"},
+            "object_score_logits": {0: "N"},
+        },
+    )
+
+    predictor.model.sam_mask_decoder.forward = ori_forward
+
+    if simplify_onnx:
+        from onnxsim import simplify
+
+        onnx_model = onnx.load(save_path)  # load onnx model
+        model_simp, check = simplify(onnx_model)
+        assert check, "Simplified ONNX model could not be validated"
+        onnx.save(model_simp, save_path.replace(".onnx", "_opt.onnx"))
+
+
+export_forward_image(override=False)
+export_image_set_image(override=False)
+export_mask_encoder(override=True)
---
+++ image.py
@@ -0,0 +1,101 @@
+import sys
+
+sys.path.insert(0, "sam2")
+
+import os
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from PIL import Image
+import cv2
+from ytools.bench import test_torch_cuda_time
+import shutil
+
+
+def draw_mask(img, mask):
+    mask = mask[..., 0:1]
+    img[:, :, 0:1] = (
+        img[:, :, 0:1] * (1 - mask) + img[:, :, 0:1] * mask * 0.6 + mask * 102
+    ).astype(np.uint8)
+    return img
+
+
+def save_masks(
+    img,
+    masks,
+    scores,
+    fold="masks",
+):
+    if os.path.exists(fold):
+        shutil.rmtree(fold)
+    os.makedirs(fold, exist_ok=True)
+    total_mask = np.zeros_like(img)[..., 0:1]
+    for i, (mask, score) in enumerate(zip(masks, scores)):
+        masked_save_path = os.path.join(fold, f"{i}_{score:.2f}_gen.jpg")
+        mask_save_path = os.path.join(fold, f"{i}_{score:.2f}_mask.jpg")
+        mask = np.expand_dims(mask.astype(np.uint8), axis=2)
+
+        img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        img_bgr = draw_mask(img_bgr, mask)
+
+        total_mask = total_mask * (1 - mask) + mask
+        cv2.imwrite(mask_save_path, mask * 255)
+        cv2.imwrite(masked_save_path, img_bgr)
+
+    masked_save_path = os.path.join(fold, f"gen.jpg")
+    mask_save_path = os.path.join(fold, f"mask.jpg")
+    img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    img_bgr = draw_mask(img_bgr, mask)
+    cv2.imwrite(mask_save_path, total_mask * 255)
+    cv2.imwrite(masked_save_path, img_bgr)
+
+
+device = torch.device("cuda")
+from sam2.build_sam import build_sam2
+from sam2.sam2_image_predictor import SAM2ImagePredictor
+
+# large
+sam2_checkpoint = "./sam2/checkpoints/sam2.1_hiera_large.pt"
+model_cfg = "configs/sam2.1/sam2.1_hiera_l.yaml"
+
+# # tiny
+# sam2_checkpoint = "./sam2/checkpoints/sam2.1_hiera_tiny.pt"
+# model_cfg = "configs/sam2.1/sam2.1_hiera_t.yaml"
+
+sam2_model = build_sam2(model_cfg, sam2_checkpoint, device=device)
+predictor = SAM2ImagePredictor(sam2_model)
+
+image = Image.open("./sam2/notebooks/images/truck.jpg")
+image = np.array(image.convert("RGB"))
+
+input_point = np.array([[500, 375]])
+input_label = np.array([1])
+
+
+@test_torch_cuda_time()
+def run(predictor: SAM2ImagePredictor, image, input_point, input_label):
+    # predictor.set_image(image)
+    # masks, scores, logits = predictor.predict(
+    #     point_coords=input_point,
+    #     point_labels=input_label,
+    #     multimask_output=True,
+    # )
+    
+    predictor.set_image(image)
+    masks, scores, logits = predictor.predict(
+        point_coords=np.array([[575, 750]]),
+        point_labels=np.array([0]),
+        box=np.array([425, 600, 700, 875]),
+        multimask_output=True,
+    )
+    
+    sorted_ind = np.argsort(scores)[::-1]
+    masks = masks[sorted_ind]
+    scores = scores[sorted_ind]
+    logits = logits[sorted_ind]
+    return masks, scores
+
+
+for _ in range(5):
+    masks, scores = run(predictor, image, input_point, input_label)
+save_masks(image, masks, scores, "data/test_image")
