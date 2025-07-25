540e92d345b300a3843e628f3a1b1ff239696d01
oneflyingfish <a_flying_fish@outlook.com>
2025-07-25 08:58:21 +0000

    add dataset download script

---
+++ datasets/download_image.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -o image.zip https://shanhai-ai.bd.bcebos.com/v1/FastProcess/CalibrationSet/sam2/image.zip?authorization=bce-auth-v1%2FALTAKKDx9mToS4enALDWmlY4yl%2F2025-07-25T08%3A43%3A43Z%2F-1%2Fhost%2F7001bbfca311438e209f2de5c984786fe8d6a08732f4389da7ab9ea06fd6a9b9
---
+++ datasets/download_video.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -o video.zip https://shanhai-ai.bd.bcebos.com/v1/FastProcess/CalibrationSet/sam2/video.zip?authorization=bce-auth-v1%2FALTAKKDx9mToS4enALDWmlY4yl%2F2025-07-25T08%3A57%3A11Z%2F-1%2Fhost%2F9b5b467da035f114ea8e04c51e606b49ad862d777f594a54b75b6740838dcae7
